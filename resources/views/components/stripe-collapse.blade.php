2f541429bce26d526b316d8139e3819935a8311f
Unlimited23 <petyoruzhin@gmail.com>
2021-08-29 19:04:37 +0300

    add stripe form element

--- resources/views/components/stripe-collapse.blade.php
+++ resources/views/components/stripe-collapse.blade.php
@@ -1,4 +1,31 @@
-<small>@lang('dashboard.payments.redirect', ['platform' => 'Stripe'])</small>
-<input type="text" name="text1">
-<input type="text" name="text2">
+<label class="mt-3" for="card-element">
+    Card details:
+</label>
 
+<div id="cardElement">
+    
+</div>
+
+<small class="form-text text-muted" id="cardErrors" role="alert"></small>
+
+@push('scripts')
+<script src="https://js.stripe.com/v3/"></script>
+
+<script>
+    const stripe = Stripe(`${{{ config('services.stripe.key') }}}`);
+    
+    const elements = stripe.elements({ locale: 'en' });
+    
+    var style = {
+        base: {
+            // Add your base input styles here. For example:
+            fontSize: '16px',
+            color: '#32325d',
+        },
+    };
+    
+    const cardElement = elements.create('card', {style});
+    
+    cardElement.mount('#cardElement');
+</script>
+@endpush
